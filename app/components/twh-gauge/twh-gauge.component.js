34241fccd9d9aeae8f672c98e552f477cab1daf4
mattstryfe <purebias@gmail.com>
2017-11-25 21:22:23 -0500

    calcQuantitativePrecip
    calcQuantitativePrecip is complete.  It's now being rendered in the UI.  Time to begin work on the gauge now that the data is ready.

--- app/components/twh-gauge/twh-gauge.component.js
+++ app/components/twh-gauge/twh-gauge.component.js
@@ -4,12 +4,23 @@ angular.module('myApp').component('twhGauge', {
   templateUrl: 'components/twh-gauge/twh-gauge.html',
   controller: TwhGaugeController,
   bindings: {
-    precip: '='
+    precip: '<'
   }
 });
 
 function TwhGaugeController () {
   var ctrl = this;
+  const precip = ctrl.precip
 
-  console.log('it works!')
+  // calculate qauntitativePrecip
+  // this will be used for the water gauge
+  ctrl.calcQuantitativePrecip = function(precip) {
+    let precipTotal = 0;
+    precip.forEach((entry) => {
+      precipTotal += entry.value
+    })
+    ctrl.precipTotal = precipTotal //make available to UI.
+  }
+
+  ctrl.calcQuantitativePrecip(precip)
 }
